d5f99c8fe30482fe5bca33a703660e51ff3bd00a
Zohaib Hassan <zohaibiqbal3800@gmail.com>
2023-09-26 00:48:35 +0500

    Top-bar, left-bar, Right-bar, Bottom-bar

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,7 @@
+export default async function () {
+    return (
+        <main>
+            <h1 className="head-text">Onboarding</h1>
+        </main>
+    )
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+
+ 
+export default function Home() {
+  return (
+      <div>
+        <div className="head-text text-left">Home</div>
+      </div>
+    
+  )
+}
