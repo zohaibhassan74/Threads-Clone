a2735c7b63451343164c5ff3c939af88352ec8d0
Zohaib Hassan <zohaibiqbal3800@gmail.com>
2023-10-08 00:38:02 +0500

    Fetch Posts on HomePage

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -3,6 +3,7 @@ import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { revalidatePath } from "next/cache";
+import { path } from 'path';
 
 interface Params {
     text: string;
@@ -33,3 +34,27 @@ export async function createThread({ text, author, communityId, path }: Params) 
     }
     
 }
+
+export async function fetchPosts(pageNumber=1, pageSize=2) {
+        connectToDB();
+        const skipAmount = pageSize * (pageNumber - 1)
+        //Fetch posts who have no parents(top level threads)
+        const postQuery = await Thread.find({
+            parentId: {
+                $in: [null, undefined],
+            }})
+            .sort({ createdAt: 'desc' }) // New one will comes up first
+            .skip(skipAmount)
+            .limit(pageSize)
+            .populate({ path: 'author', model: User })
+            .populate({
+                path: 'children', populate: {
+                    path: 'author', model: User,
+                    select:"_id name parentId image"
+                }})
+                
+        const totalPosts = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+     const posts = await postQuery;
+     const isNextPage = totalPosts > pageSize * pageNumber
+     return { posts, isNextPage }
+    }
