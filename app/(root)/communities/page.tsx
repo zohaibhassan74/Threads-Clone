ea5242b4e8e1610ded055577a65b3128c72fa952
Zohaib Hassan <zohaibiqbal3800@gmail.com>
2023-10-16 23:45:05 +0500

    Community

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,4 +1,4 @@
-const search = async () => {
+const communities = async () => {
     return(
         <section>
             <h1 className='head-text mb-10'>Communities</h1>
@@ -6,4 +6,4 @@ const search = async () => {
     )
 }
 
-export default search
+export default communities
