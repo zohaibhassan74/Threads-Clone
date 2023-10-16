125814accf23683d6ca9da951264841157b9b44f
Zohaib Hassan <zohaibiqbal3800@gmail.com>
2023-10-17 00:44:34 +0500

    Adding the userInfo

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,15 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
     const user = await currentUser()
-    const userInfo = {}
+    if (!user) return null
+    
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
+    // const userInfo = {}
     const userData = {
         id: user?.id,
         objectid: userInfo?._id || "",
